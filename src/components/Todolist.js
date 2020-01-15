29b4b1ab971f5fd1e69c265599068e86ac8b823c
Antoine Chan <achantchisong@mailjet.com>
2020-01-15 16:45:45 +0100

    Init project, add Todolist component
    tips:
    - Render content for React component.
    - Naming convention component.

---
+++ src/components/Todolist.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Todolist = () => {
+  return <ul>
+    <li>item 1</li>
+    <li>item 2</li>
+    <li>item 3</li>
+    <li>item 4</li>
+    <li>item 5</li>
+  </ul>
+}
+
+export default Todolist
