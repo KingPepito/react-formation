13b444aeb26e6da85002d0042f3efc50540bdce7
Antoine Chan <achantchisong@mailjet.com>
2020-01-16 16:45:50 +0100

    Add Task.css, apply style already use in Task component with css. Edit App.css, color task completed in red.
    tips:
    - Condition bind with back tick in string '`${...js...}`'.
    - Import and use css class is breaking component principle and making it harder to maintain.

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Task.css";
 
 const Task = ({complete, completed, id, title, remove}) => {
   const removeSelf = () => remove(id)
   const completeSelf = () => complete(id)
-  return <div style={{display: 'flex'}}>
-    <span style={{margin: '8px', cursor: "pointer", textDecoration: completed && 'line-through'}} onClick={completeSelf}>
+  return <div className='task-container'>
+    <span className={`task-title ${completed && 'completed'}`} onClick={completeSelf}>
       {title}
     </span>
     <button onClick={removeSelf}>X</button>
