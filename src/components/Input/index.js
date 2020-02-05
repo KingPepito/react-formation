8c92c85a105ac580cfe6c015fcacf83d3e5e8512
Antoine Chan <antoinectsmad@gmail.com>
2020-02-05 11:10:58 +0100

    Refactor HomePage login uncontrolled form into a controlled form. Refactor Input, allowing custom border color passed as a prop.
    tips:
    - Controlled form allow you to provide complex validation step while typing
    in an input.

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -4,21 +4,29 @@ import styled from "styled-components"
 
 const StyledInput = styled.input`
   margin-bottom: 16px;
+  padding: 4px;
+  border-radius: 4px;
+  ${({color}) => color && `border: solid 2px ${color};`}
+  outline: none;
 `
 
-const Input = ({onSubmit}) => {
+StyledInput.propTypes = {
+  color: PropTypes.string,
+}
+
+const Input = ({onSubmit, ...rest}) => {
   // Prefer ref to id
   const inputEl  = useRef(null)
   // Event fired when typing, execute onSubmit when pressing enter
   const onKeyDown = e => {
-    if(e.key === 'Enter' && inputEl.current.value !== "") {
+    if(e.key === 'Enter' && inputEl.current.value !== "" && onSubmit) {
       onSubmit(e.target.value)
       // Reset value
       inputEl.current.value = ""
     }
   }
 
-  return <StyledInput onKeyDown={onKeyDown} ref={inputEl}/>
+  return <StyledInput {...rest} onKeyDown={onKeyDown} ref={inputEl}/>
 }
 // Provide property validation to your component
 Input.propTypes = {
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,32 +1,52 @@
-import React, {useRef} from 'react'
+import React, {useState} from 'react'
 import {useHistory} from "react-router-dom";
-import {get} from "lodash";
 import axios from "axios";
 import HomeContainer from "./styles/HomeContainer";
 import LoginForm from "./styles/LoginForm";
+import {validateEmail} from "../../helpers/isEmailValid";
+import Input from "../../components/Input";
 
 const Home = () => {
   const history = useHistory();
   // Creating a ref for each input in order to get the value of the input when submitting the form.
   // The related react form is non-controlled.
-  const loginInput = useRef()
-  const passwordInput = useRef()
+  const [login, setLogin] = useState({value: '', color: null})
+  const [password, setPassword] = useState('')
+
+  const red = 'rgba(255,31,21,0.82)',
+    green = 'rgba(103,203,79,0.84)'
+
+  const handleChangeLogin = (event) => {
+    const {value} = event.target
+    let color = null
+    // Data validation, setting the login input color to red or green
+    if(value){
+      color = validateEmail(value) ? green : red
+    }
+    setLogin({value, color});
+  }
+
+  const handleChangePassword = (event) => {
+    setPassword(event.target.value);
+  }
 
   const onSubmit = event => {
+    const email = login.value
     // Prevent page to refresh
     event.preventDefault();
     event.stopPropagation();
-    const email = get(loginInput, 'current.value')
-    const password = get(passwordInput, 'current.value')
-    // HERE check data validation
-    // ...
+    if(!email || !password || !validateEmail(email)){
+      // Notify the user that Email format is not valid
+      console.log("Email format is not valid, please provide an correct email while logging.")
+      return
+    }
     // Use a axios promise
     axios.post("https://reqres.in/api/login", {
-      email, // same as "email": email
-      password
+      email,
+      password // same as password: password
     }).then(res => {
-      // Add session cookie, very short duration for the course purpose
-      document.cookie = `idUser=${res.data.token}; max-age=5`
+      // Add session cookie
+      document.cookie = `idUser=${res.data.token}; max-age=7200`
       // Go to sample Overview page using history react-router hook
       history.push('/overview')
     })
@@ -38,8 +58,8 @@ const Home = () => {
   return <HomeContainer>
     <h1>Welcome to my Todo list app</h1>
     <LoginForm onSubmit={onSubmit}>
-      <input ref={loginInput} placeholder='login'/>
-      <input ref={passwordInput} type='password' placeholder='password'/>
+      <Input placeholder='login' color={login.color} onChange={handleChangeLogin}/>
+      <Input type='password' placeholder='password' onChange={handleChangePassword}/>
       <button type="submit">Login</button>
     </LoginForm>
   </HomeContainer>
