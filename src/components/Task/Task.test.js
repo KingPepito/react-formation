1066f4d619195acf0510a59c4e391439a285c099
Antoine Chan <antoinectsmad@gmail.com>
2020-04-01 16:09:25 +0200

    Install packages enzyme and enzyme-adapter-react-16". Configure enzyme setup in setupTests.js file. Add some DOM testing with Enzyme for Task component.
    Tips:
    - Enzyme is a React components testing library. It can be coupled with Jest as it provides more utilities to test your React components. It can provide more specificity than a simple rendering test.
    - "shallow" is one of the methods that use Enzyme to render a component. It is used to test a component as a single unit, not handling the potential children related.
    - Sometimes you might want to choose the important features that are testing-worth instead of test every single part of a component.
    - Enzyme needs a small configuration provided in setupTest.js file.
    - When testing a component's child node, you might want to be very specific by using a custom attribute to identify the node you are looking for.

--- src/components/Task/Task.test.js
+++ src/components/Task/Task.test.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import Task from './';
 import {renderWithTheme} from "../../helpers/renderWithTheme";
+import {shallow} from "enzyme";
+import TaskTitle from "./styles/TaskTitle";
+
+
 // We simply testing that our component Task is rendering here, snapshot testing might also include more features...
 // Each time we run a snapshot test, it will create a snapshot files inside __snapshots__.
 // Snapshot test will notify if the HTML produced by our component has been changes since the last time we run the test.
-// If it is wanted that our changes will modify the HTML output, we shall update the tests by using 'yarn test -u'
-describe("Task", () => {
+// If it is wanted that our changes will modify the HTML output, we shall update the tests by using 'yarn test -u'.
+describe("Task snapshot test.", () => {
   // "describe" is use to creates a block that groups together several related tests. It is not mandatory but can make the test more readable.
+  // "test" is used to assert that the function passed as argument is executed correctly, first arg is describing the test.
   test('Simply test if the component is rendering', () => {
-    const component = renderWithTheme(<Task title="Hey I'm a test task!"/>);
+    const component = renderWithTheme(setupTask({title:"Hey I'm a test task!"}));
     let tree = component.toJSON();
     // "expect" like "describe" is a function related to Jest, it might looks unintuitive but no need to import it here.
-    // Jest will make sure when running that this kind of functions are imported
+    // Jest will make sure when running that this kind of functions are imported.
     expect(tree).toMatchSnapshot();
   });
+})
+// "shallow" is provided by Enzyme, it renders only the single component, not including its children.
+// This is useful to isolate the component for pure unit testing.
+const setupTask = (props = {}) => shallow(<Task {...props}/>)
+
+describe("Task DOM testing with Enzyme.", () => {
+  let componentToTest
+  const title = "Hey I'm a second test task!"
+  // "beforeEach" is a function that Jest will run before each test of the current block.
+  beforeEach(() => {
+    // Initialize the Task component with some props.
+    componentToTest = setupTask({title})
+  })
+  // "it" is a alias for "test" function, it allows us to read the test call as "it should render a remove button".
+  // Here we assume that the remove button is a really important Task component's feature and its presence need to be test.
+  it('should render a remove button', () => {
+    // This will prove that our Task component contains a Button.
+    expect(componentToTest.find('button'))
+    // But we might want to be more specific by using a custom attribute to identify the node we are looking for.
+    expect(componentToTest.find("[data-test='remove-button-task-test']"))
+  })
+
+  it('should render a title with the expected text', () => {
+    // We might find an imported sub-component like this:
+    const titleCompo = componentToTest.find(TaskTitle);
+    expect(titleCompo)
+    expect(titleCompo.text()).toContain(title)
+    expect(titleCompo.text()).not.toContain('what ever text not expected')
+  })
 })
